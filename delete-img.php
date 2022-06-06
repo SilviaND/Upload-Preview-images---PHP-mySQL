97605252102388df7f689e96bf37b0ada74f528f
SilviaND <105547682+SilviaND@users.noreply.github.com>
2022-06-06 10:54:03 +0300

    Add files via upload

--- delete-img.php
+++ delete-img.php
@@ -1,25 +1,21 @@
 <?php
 require "connection.php";
 
-//$id=$_GET["id"];
-//
-//$res=mysqli_query("SELECT * from images WHERE id=$id");
-//while($row=mysqli_fetch_array($res)){
-//    $img=$row["image"];
-//}
-//unlink($img);
-//
-//mysqli_query("DELETE FROM images where id=$id");
+$id=$_GET["id"];
 
-$db = "DELETE FROM images WHERE id=$id";
+$sql = "DELETE FROM images WHERE id=$id";
 
-if ($conn->query($db) === TRUE) {
+if ($db->query($sql) === TRUE) {
     echo "Record deleted successfully";
 } else {
-    echo "Error deleting record: " . $conn->error;
+    echo "Error deleting record: " . $db->error;
 }
 
-$conn->close();
+unlink($id);
+
+$db->close();
+
+
 ?>
 
 <script>
